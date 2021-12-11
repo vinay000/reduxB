3e2b272a04e5a737a8699d2508cceb213cf09869
Vinay <36290283+vinay000@users.noreply.github.com>
2021-12-11 16:27:02 +0530

    Update 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { useSelector } from 'react-redux';
 import Home from './increments/Home';
-import Test from './test/Test';
 function App() {
 const myState = useSelector((state) => state.changeNumber)
   return (
     <div className="App">
-      {/* <Home currentValue={myState}/> */}
-      <Test/>
+      <Home currentValue={myState}/>
     </div>
   );
 }
